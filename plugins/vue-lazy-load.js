0dab4a2dba4f2cd5a602c38aaac4ce7ffa82ce4c
Cher Stewart <cstewart@cstewart017338.starbucks.net>
2018-05-02 00:12:05 -0500

    loader rename

--- plugins/vue-lazy-load.js
+++ plugins/vue-lazy-load.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import VueLazyload from 'vue-lazyload'
+import Vue from "vue";
+import VueLazyload from "vue-lazyload";
 
 Vue.use(VueLazyload, {
-  loading: 'loader.svg',
-  error: 'loader.svg',
+  loading: "loading.svg",
+  error: "loading.svg",
   attempt: 10,
   throttleWait: 1000,
   lazyComponent: true
-})
+});
